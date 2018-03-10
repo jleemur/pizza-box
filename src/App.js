a7eb70931365a578caff6f5fa49a362b30120216
alex <brewster.aj77@gmail.com>
2018-03-10 17:06:08 -0400

    the start of the start page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="Pizza-Box">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <span>
+            <img src={require('./pizzabox.png')} className="App-logo" />
+          </span>
+          <h1 className="App-title">Pizza-Box</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Start />
       </div>
     );
   }
 }
+// //////////////////////////////////////////////////////////
+
+class Start extends React.Component {
+// NEEED TO FIGURE THIS OUT LOLOL
+
+
+  // constructor(props) {
+  //   super(props);
+  //   this.handleStartClick = this.handleStartClick.bind(this);
+  //   this.handleAddPlayersClick = this.handleAddPlayersClick.bind(this);
+  //   this.state = { isStart: false };
+  // }
+
+  // handleStartClick() {
+  //   this.setState({ isStart: true });
+  // }
+
+  // handlePlayersClick() {
+  //   this.setState({ addPlayers: false });
+  // }
+
+  render() {
+  //   const isStart = this.state.isStart;
+
+  //   if (isStart) {
+  //     button = <AddPlayersButton onClick={this.handleAddPlayersClick} />;
+  //   } else {
+  //     button = <StartButton onClick={this.handleStartClick} />;
+  //   }
+
+    return (
+      <button className="Start" onClick={e => this.handleClick(e)}>
+        Start Game!
+      </button>
+    );
+  }
+}
+
 
 export default App;
+
+// /////////////////////////////////////////////////////////////
