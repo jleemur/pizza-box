1fe88bb499d6b766dc6b90062330061bf6673bbb
Jonathan Murray <jonathan.murray1@ibm.com>
2019-01-24 00:42:44 -0400

    Sociables game, no css

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const RULES = [
+  'Give a drink',
+  'Give 2 drinks',
+  'Sociables',
+  'Fuzzy duck',
+  'Story time',
+  'Rule',
+  'Question master',
+  'Thumb master',
+  'Rule',
+  'Never have I ever',
+  'Waterfall',
+  'Girls drink',
+  'Guys drink'
+];
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rule: ""
+    };
+
+    this.nextRule = this.nextRule.bind(this);
+  }
+
+  nextRule() {
+    const num = Math.round(Math.random() * 12); // 0-12
+    this.setState({rule: RULES[num]});
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{this.state.rule}</Text>
+        <Button title='Next' onPress={this.nextRule}>Next</Button>
       </View>
     );
   }
